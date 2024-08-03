e68f67423d6997869a223d24ab83eb3a1c5ed450
Justin S Gonzales <JustinMusicMan24@gmail.com>
2024-08-03 15:30:37 -0700

    running, walking, and acceleration

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -3,25 +3,31 @@ extends CharacterBody2D
 @onready var sprite_2d = $Sprite2D
 @onready var animation_player = $AnimationPlayer
 
-@export var speed = 300.0
-@export var jump_velocity = -400.0
+@export var max_walking_speed = 80.0
+@export var max_running_speed = 160.0
+
+@export var acceleration = 5.0
+var deceleration = 5.0
+
+var current_max_speed = 0.0
+
+@export var jump_velocity = -350.0
 
 var can_move = true
 var is_jumping = false
+var is_running = false
 var facing_direction = 1
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
 func _physics_process(delta):	
+	# Get the input direction
+	var direction = Input.get_axis("move_left", "move_right")
+	
 	# Add the gravity.
 	if not is_on_floor():
 		velocity.y += gravity * delta
-
-	if can_move == false:
-		velocity.x = 0
-		move_and_slide()
-		return
 
 	# Handle jump.
 	if Input.is_action_pressed(&"jump") and is_on_floor():
@@ -29,14 +35,32 @@ func _physics_process(delta):
 		is_jumping = true
 	elif is_on_floor():
 		is_jumping = false
-
-	# Get the input direction
-	var direction = Input.get_axis("move_left", "move_right")
 	
+	# Running and walking
+	if Input.is_action_pressed(&"run"):
+		current_max_speed = max_running_speed
+		# current_deceleration = running_deceleration
+		# current_acceleration = running_acceleration
+		is_running = true
+	else:
+		current_max_speed = max_walking_speed
+		# current_deceleration = walking_deceleration		
+		# current_acceleration = walking_acceleration
+		is_running = false
+	
+	# Apply movement	
+	if direction:
+		if abs(velocity.x) > current_max_speed:
+			velocity.x -= sign(velocity.x) * deceleration
+		else:
+			velocity.x += direction * acceleration
+	else:
+		velocity.x = move_toward(velocity.x, 0, deceleration)
+
+	# Flip sprite	
 	if direction != 0:
 		facing_direction = direction
 	
-	# Flip sprite
 	sprite_2d.flip_h = facing_direction < 0
 		
 	# Play animations
@@ -44,16 +68,15 @@ func _physics_process(delta):
 		animation_player.stop()
 		sprite_2d.frame = 3
 	else:
+		# idle
 		if direction == 0:
 			animation_player.stop()
 			sprite_2d.frame = 1
-		else:
-			animation_player.play(&"run")
+		else: # running or walking
+			if is_running:
+				animation_player.play(&"run")
+			else:
+				animation_player.play(&"walk")
 			
-	# Apply movement
-	if direction:
-		velocity.x = direction * speed
-	else:
-		velocity.x = move_toward(velocity.x, 0, speed)
 
 	move_and_slide()
